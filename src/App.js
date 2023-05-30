5a1ec151fbce36853ae87775c93308ecfea44195
Alperen HArmankaşı <114261641+Alperenhks@users.noreply.github.com>
2023-05-30 23:04:54 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// Alperen Harmankaşı tarafından hazırlanmıştır.
+
 import React, { useState, useRef } from 'react';
 import QRCode from 'qrcode.react';
 import './App.css';
@@ -12,8 +14,11 @@ function App() {
   };
 
   const handleGenerateQRCode = () => {
+    if (link.trim() === '') {
+      alert('Lütfen bir bağlantı giriniz.');
+      return;
+    }
     setShowQRCode(true);
-    
   };
 
   const handleDownloadQRCode = () => {
@@ -28,7 +33,9 @@ function App() {
   return (
     <div className="container my-5">
       <div className="card p-4 bg-dark text-white">
-        <h1 className="card-header text-center mb-4">QR Code Generator</h1>
+        <h1 className="card-header text-center mb-4" style={{ fontFamily: 'Pacifico, cursive' }}>
+          QR Kod Oluşturucu
+        </h1>
         <div className="card-body d-flex flex-column align-items-center">
           <div className="form-group w-75">
             <input
@@ -36,22 +43,26 @@ function App() {
               value={link}
               onChange={handleInputChange}
               className="form-control"
-              placeholder="Enter a link"
+              placeholder="Bir web sitesi girin"
             />
           </div>
           <button className="btn btn-primary w-75" onClick={handleGenerateQRCode}>
-            Generate QR Code
+            QR Kodu Oluştur
           </button>
           {showQRCode && (
             <div className="text-center mt-4" ref={qrCodeRef}>
               <QRCode value={link} />
               <button className="btn btn-primary mt-3" onClick={handleDownloadQRCode}>
-                Download QR Code
+                QR Kodunu İndir
               </button>
             </div>
           )}
-        </div>
+         
+        </div>  <div className="mdt-4">
+            <p className="">Alperen Harmankaşı tarafından hazırlanmıştır.</p>
+          </div>
       </div>
+     
     </div>
   );
 }
